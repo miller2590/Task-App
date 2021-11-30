2db35eabbd24036543b063221dd56a0435c96ab2
miller2590 <miller2590@gmail.com>
2021-11-29 19:53:07 -0600

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TaskList from "../TaskList";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <TaskList />;
+  }
+}
+
+export default App;
---
+++ src/Components/TaskList/TaskLIst.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class TaskLIst extends Component {
+  render() {
+    return <div>Hi</div>;
+  }
+}
+
+export default TaskLIst;
